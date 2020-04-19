81e8e4595c27ab559195bb4b289ae6fa03f66e55
Justin T. Conroy <justin@conroy.in>
2020-04-19 18:44:39 -0500

    Starting a new app.

---
+++ light-control-client/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Light Control!</h1>
+      </header>
+      <body>
+        <div></div>
+      </body>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
