f32a89669ec4263e0f30eafb851451b8ba32b71a
Justin T. Conroy <justin@conroy.in>
2020-04-19 17:40:41 -0500

    Starting over with the light things.

--- LightControl/ClientApp/src/App.tsx
+++ LightControl/ClientApp/src/App.tsx
@@ -4,7 +4,6 @@ import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
-import { LightManager } from "./components/LightManager";
 
 import "./custom.css";
 
@@ -17,7 +16,6 @@ export default class App extends React.Component {
         <Route exact path="/" component={Home} />
         <Route path="/counter" component={Counter} />
         <Route path="/fetch-data" component={FetchData} />
-        <Route path="/light-manager" component={LightManager} />
       </Layout>
     );
   }
