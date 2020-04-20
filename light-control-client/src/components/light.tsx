4aa2fd7003719944f811afdd20ca9709f7399e32
Justin T. Conroy <justin@conroy.in>
2020-04-19 19:45:44 -0500

    Hilight selected light.

--- light-control-client/src/components/light.tsx
+++ light-control-client/src/components/light.tsx
@@ -4,36 +4,38 @@ import { Color, ColorResult, SketchPicker } from "react-color";
 interface ILightProps {}
 interface ILightState {
   color: Color;
-  showPicker: boolean;
+  isSelected: boolean;
 }
 
 export class Light extends React.Component<ILightProps, ILightState> {
   constructor(props: ILightProps) {
     super(props);
-    this.state = { color: "#000000", showPicker: false };
+    this.state = { color: "#000000", isSelected: false };
   }
   handleChange = (color: ColorResult) => {
     this.setState({ color: color.hex });
   };
 
   handleClick = () => {
-    this.setState({ showPicker: !this.state.showPicker });
+    this.setState({ isSelected: !this.state.isSelected });
   };
 
   render() {
     return (
       <div>
-        <svg>
+        <svg className="SingleLight">
           <circle
             cx={50}
             cy={50}
             r={20}
+            stroke="black"
+            stroke-width={this.state.isSelected ? "4" : 0}
             fill={this.state.color as string}
             onClick={this.handleClick}
           />
         </svg>
-        {this.state.showPicker && (
-          <div>
+        {this.state.isSelected && (
+          <div className="SingleLight-Picker">
             <SketchPicker
               color={this.state.color}
               disableAlpha={true}
